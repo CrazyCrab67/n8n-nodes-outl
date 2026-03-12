f21d01125f02f4591434032f857d499e6125300b
JM <jmorasch@symplasson.de>
2026-03-12 15:38:54 +0000

    fix

--- nodes/outline/outline.node.ts
+++ nodes/outline/outline.node.ts
@@ -31,7 +31,7 @@ export class outline implements INodeType {
                 Accept: 'application/json',
                 'Content-Type': 'application/json',
             },
-            baseURL: '{{$credentials.url}}',
+            baseURL: '{{$credentials.baseUrl}}',
         },
         properties: properties,
     };
